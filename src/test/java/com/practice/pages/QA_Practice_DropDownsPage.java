1c5bf9100e52f1f65f69a75971523224d894cf94
ersu-amine <ersu-amine@gmail.com>
2025-01-13 17:53:27 -0500

    Dropdown practice 2

--- src/test/java/com/practice/pages/QA_Practice_DropDownsPage.java
+++ src/test/java/com/practice/pages/QA_Practice_DropDownsPage.java
@@ -14,5 +14,25 @@ public class QA_Practice_DropDownsPage {
     @FindBy(xpath = "//select[@id='dropdown-menu']")
     public WebElement selectDropdown;
 
+    @FindBy(css = "div.dropdown>button")
+    public  WebElement multiLevelDropdown;
+
+    @FindBy(xpath = "//a[text() = 'Some other action']")
+    public WebElement option2;
+
+    @FindBy(xpath = "//a[text() = 'Hover me for more options']")
+    public WebElement option3;
+
+    @FindBy(xpath = "//a[text() = 'Even More..']")
+    public WebElement secondLevelDropdown;
+
+    @FindBy(xpath = "//a[text() = 'another level']")
+    public WebElement thirdLevelDropdown;
+
+    @FindBy(xpath = "//a[@href = '#4th-level-1']")
+    public WebElement fourthLevelOption1;
+
+
+
 
 }
--- src/test/java/com/practice/tests/Dropdowns.java
+++ src/test/java/com/practice/tests/Dropdowns.java
@@ -4,20 +4,49 @@ import com.practice.library.BrowserUtilities;
 import com.practice.library.ConfigurationReader;
 import com.practice.library.Driver;
 import com.practice.pages.QA_Practice_DropDownsPage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 public class Dropdowns {
     QA_Practice_DropDownsPage dropDownsPage = new QA_Practice_DropDownsPage();
 
+    Logger LOG = LogManager.getLogger();
+
     @Test
-    void dropdowns() {
+    void simpleDropdowns() throws InterruptedException {
         Driver.getDriver().get(ConfigurationReader.getProperty("qa_practice") + "/dropdowns");
 
         Select dropdown = new Select(dropDownsPage.selectDropdown);
 
         dropdown.selectByValue("Taiwan");
+        LOG.info("selecting by value");
         dropdown.selectByVisibleText("Fiji");
+        LOG.info("selecting by visible text");
         dropdown.selectByIndex(29);
+        LOG.info("selecting by index");
+
+        BrowserUtilities.sleep(2);
+        Driver.getDriver().quit();
+    }
+
+    @Test
+    void multiLevelDropdowns() throws InterruptedException {
+        Driver.getDriver().get(ConfigurationReader.getProperty("qa_practice") + "/dropdowns");
+
+        dropDownsPage.multiLevelDropdown.click();
+        dropDownsPage.option2.click();
+        LOG.info("Option 2 has been chosen");
+
+        //needs hovering
+        Actions actions = new Actions(Driver.getDriver());
+        dropDownsPage.multiLevelDropdown.click();
+        actions.moveToElement(dropDownsPage.option3).moveToElement(dropDownsPage.secondLevelDropdown).moveToElement(dropDownsPage.thirdLevelDropdown).moveToElement(dropDownsPage.fourthLevelOption1).perform();
+        dropDownsPage.fourthLevelOption1.click();
+        LOG.info("Fourth level option 1 has been selected");
+        BrowserUtilities.sleep(2);
+        Driver.getDriver().quit();
     }
 }
