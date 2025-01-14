a04f38acecdb415f59ad1b61c489a36f9130ef76
ersu-amine <ersu-amine@gmail.com>
2025-01-13 22:59:20 -0500

    Add method for explicitly waiting for element visibility

--- src/test/java/com/practice/library/BrowserUtilities.java
+++ src/test/java/com/practice/library/BrowserUtilities.java
@@ -1,12 +1,15 @@
 package com.practice.library;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BrowserUtilities {
-
 
     /**
      *
@@ -18,5 +21,8 @@ public class BrowserUtilities {
         Thread.sleep(time);
     }
 
+    public static void waitForElementVisibility(WebElement element){
+        new WebDriverWait(Driver.getDriver(),Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(element));
+    }
 
 }
