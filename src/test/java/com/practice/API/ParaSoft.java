4d126db4f7852f0627782b70022181832298ac83
ersu-amine <ersu.amine@gmail.com>
2025-03-17 01:38:00 -0400

    Practice API call

---
+++ src/test/java/com/practice/API/ParaSoft.java
@@ -0,0 +1,31 @@
+package com.practice.API;
+
+import com.practice.library.ConfigurationReader;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ParaSoft {
+
+    @Test
+    public void test() {
+        //https://parabank.parasoft.com/parabank/index.htm
+        RestAssured.baseURI = ConfigurationReader.getProperty("parasoft_API");
+
+        //login
+        Map <String, String> pathParams = new HashMap<>();
+        pathParams.put("username", ConfigurationReader.getProperty("username"));
+        pathParams.put("password", ConfigurationReader.getProperty("password"));
+
+        //Response response = RestAssured.given().accept(ContentType.JSON).and().pathParam("username", ConfigurationReader.getProperty("username")).and().pathParam("password", ConfigurationReader.getProperty("password")).log().all().when().get("/login/{username}/{password}");
+
+        Response response = RestAssured.given().accept(ContentType.JSON).
+                and().pathParams(pathParams).log().all().when().get("/login/{username}/{password}");
+
+        response.prettyPrint();
+    }
+}
