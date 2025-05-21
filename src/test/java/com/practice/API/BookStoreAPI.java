1151da43c27cbc95990eee664860c423c00e53c0
ersu-amine <ersu.amine@gmail.com>
2025-05-21 00:32:38 -0400

    BookStore API rename and add getToken method

--- src/test/java/com/practice/API/BookStoreAPI.java
+++ src/test/java/com/practice/API/BookStoreAPI.java
@@ -2,12 +2,14 @@ package com.practice.API;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-public class BookerAPI {
+public class BookStoreAPI {
     //https://demoqa.com/books
     //https://demoqa.com/swagger/#/Account/AccountV1UserPost
 
@@ -16,6 +18,25 @@ public class BookerAPI {
         RestAssured.baseURI = "https://demoqa.com";
     }
 
+    private static String getToken(String username, String password){
+        Response response = RestAssured.given()
+                .accept(ContentType.JSON)
+                .and()
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/Account/v1/GenerateToken")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .extract().response();
+        response.body().prettyPrint();
+        JsonPath jsonPath = response.jsonPath();
+
+        return jsonPath.getString("token");
+
+    }
+
+    //TODO implement masking username and password
     @Test
     void createUser() {
         Map < String, String> userData = Map.of("userName","theflash543","password","Hello123@");
@@ -27,4 +48,7 @@ public class BookerAPI {
                 .post("/Account/v1/User").prettyPeek()
         .then().statusCode(201).and().contentType(ContentType.JSON);
     }
+
+
+    
 }
