a2328fb4d6feae377de9fb5d998873e31558421c
ersu-amine <ersu.amine@gmail.com>
2025-09-15 22:37:30 -0400

    API practice with Restful Booker API

---
+++ src/test/java/com/practice/API/RestfulBooker/Auth.java
@@ -0,0 +1,39 @@
+package com.practice.API.RestfulBooker;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Auth {
+
+    public static String getAuthToken() {
+        String reqBody = "{\"username\": \"admin\", \"password\": \"password123\"}";
+        Map<String, String> mapBody = new HashMap<>();
+        mapBody.put("username", "admin");
+        mapBody.put("password", "password123");
+
+        String token = "";
+        JsonPath jPath = RestAssured.given()
+                .accept(ContentType.JSON)
+                .and()
+                .contentType(ContentType.JSON)
+                .when()
+                .body(mapBody)
+                .post("/auth")
+                .then()
+                .statusCode(201)
+                .extract().jsonPath();
+
+        //jPath.prettyPrint();
+
+        token = jPath.getString("token");
+        return token;
+    }
+
+}
---
+++ src/test/java/com/practice/API/RestfulBooker/RestfulBooker.java
@@ -0,0 +1,49 @@
+package com.practice.API.RestfulBooker;
+
+import com.practice.library.ConfigurationReader;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class RestfulBooker {
+    static {
+        RestAssured.baseURI = ConfigurationReader.getProperty("restfulAPI");
+    }
+
+    @Test
+    @DisplayName("GET - Health Check")
+    public void healthCheck() {
+        //status code 201
+        Response response = RestAssured.given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("/ping")
+                .then()
+                .statusCode(201)
+                .extract().response();
+
+        response.prettyPrint();
+
+        //TODO assert response body has "Created"
+    }
+
+    @Test
+    @DisplayName("GET - Bookings list, no Auth")
+    public void getBookings(){
+        JsonPath jsonPath = RestAssured.given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("/booking")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .extract().jsonPath();
+
+        //TODO create booking, save id and assert if present
+    }
+
+
+}
